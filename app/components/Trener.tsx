08bef60f9f42e70a48a8e12bc68a76ab7e02a328
ATesic49 <atesic7@gmail.com>
2024-02-22 21:05:57 +0100

    da

--- app/components/Trener.tsx
+++ app/components/Trener.tsx
@@ -3,14 +3,14 @@ import avatar from '@/public/imgs/avatar.jpeg'
 import Image from 'next/image'
 function Trener() {
     return (
-        <div className='absolute bottom-0 left-1/2 -translate-x-1/2 flex bg-gray-100 border-gray-600 duration p-2 rounded-md z-40 gap-4 w-5/6  translate-y-1/2 justify-start items-center border-4'>
+        <div className='absolute bottom-0 left-1/2 -translate-x-1/2 flex bg-gray-100 border-gray-600 duration p-2 rounded-md z-40 gap-4 w-5/6  translate-y-1/2 justify-start items-center border-4 focus:border-orange-600 focus:scale-105 duration-200 ' tabIndex={1}>
             <Image src={avatar} alt='' className='w-12 ' />
             <div className='flex items-start justify-start flex-col  text-gray-800 text-sm'>
                 <h2 className='font-bold text-base'>Trener</h2>
                 <p className='text-xs'>Nebojša Marjanović <br /> Konan</p>
             </div>
             <div className='ml-auto text-sm'>
-                <div className=' px-2 py-1 bg-orange-500 font-bold shadow-sm '>Change</div>
+                <div className=' px-2 py-1 bg-orange-500 font-bold shadow-sm text-gray-100'>Change</div>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ import Navbar from "./components/Navbar";
 import Trener from "./components/Trener";
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center bg-gray-200 items-center w-full min-h-svh gap-8 ">
-      <Image src={tribine} fill alt="" className="object-cover object-center" quality={2}></Image>
-      <div className="relative md:aspect-[662/1000] md:w-auto md:h-[80vh] w-11/12 p-4 flex justify-center items-center rounded-2xl bg-white ">
+    <main className=" flex relative flex-col justify-center  items-center md:h-full w-full min-h-svh gap-8 ">
+      <Image src={tribine} alt="" fill className="object-cover object-center h-full" quality={2}></Image>
+      <div className="md:my-32 relative md:aspect-[662/1000] md:w-auto md:h-screen w-11/12 p-4 flex justify-center items-center rounded-2xl bg-white ">
         <div className="bg-white w-full z-30 h-1/2 bg-opacity-50 rounded-b-xl border-t-8 border-dashed absolute top-1/2 flex justify-center items-end font-bold text-4xl text-gray-400 border-gray-100 pb-8"><p className="opacity-75 text-6xl ">Klupa</p></div>
         <Image src={teren} className="h-full object-contain aspect-auto" alt=""></Image>
         <Avatar c={true} avatar={avatar} top={'top-1/2'} left={'left-1/2'} ime="Aleksa Tesic"></Avatar>
